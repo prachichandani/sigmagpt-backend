294c3542f50a9c0b8d6f84d994a48912a91b9d7e
prachi chandani <prachichandani05@gmail.com>
2026-01-07 15:38:30 +0530

    Fix cors origin and credentials

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,8 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sigmagpt-phi.vercel.app/","http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["https://sigmagpt-phi.vercel.app","http://localhost:5173"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
